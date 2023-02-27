34b3bf1066f0432fe1a27be823ab34331dbbe6ac
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2023-02-27 21:20:40 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Hello from './Hello';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+      </div>
+    );
+  }
+}
+
+export default App;
