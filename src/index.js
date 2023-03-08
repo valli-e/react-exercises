6d80bda615a7227fffadd38314ed56fff7c24f91
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2023-03-08 18:37:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './Hello';
+import App from './App';
 
 ReactDOM.render(
-  <Hello/>,
+  <App />,
   document.getElementById('root')
 );
+
 
