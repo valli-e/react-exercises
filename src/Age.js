62916a33284dc21d5ae09e8b99872bee6a6864ce
Valentyna Hnatyuk <hnatyukvalentyna01@gmail.com>
2023-03-08 22:29:16 +0100

    age component modified

--- src/Age.js
+++ src/Age.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
-class Age extends React.Component{
-    render() {
-  return (
-    <div>
-        <p>Your age is {this.props.age}</p>
-    </div>
-  )
-}
+class Age extends React.Component {
+  render() {
+    const val = this.props.age;
+    return (
+      <div>
+        {this.props.age > 18 ? <p>Your age is {this.props.age}</p> : <p>You are very young!</p>}
+      </div>
+    );
+  }
 }
