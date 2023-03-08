71d2b852023c844f970ee9be4e0bf57a036b40e9
Valentyna Hnatyuk <hnatyukvalentyna01@gmail.com>
2023-03-08 18:40:37 +0100

    main branch update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-      </div>
-    );
-  }
+export class App extends React.Component{
+    render(){
+        return <div></div>
+    }
 }
-export default App;
