86e46012c49ceef322cf1e779d797c81481fc6c0
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2023-03-08 18:36:59 +0100

    app.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import  Welcome  from './Welcome'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        < Welcome name ="Valentina" />
+      </div>
+    );
+  }
+}
+export default App;
