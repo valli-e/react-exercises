e5f77efe0d59442b4104f62b31e53f4e2e2bf8e0
Valentyna Hnatyuk <hnatyukvalentyna01@gmail.com>
2023-03-08 15:45:28 +0100

    components-07

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Message />
+    </div>
+  );
+}
+
+export default Hello;
---
+++ src/Message.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div><p>What a beautiful day!</p>;
+    </div>
+  )
+}
