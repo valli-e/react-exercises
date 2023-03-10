3a5f837ca7c0d4b58c6aaeca0139bc7bbc84d0c0
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2023-03-10 21:05:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component{
   render() {
     return (
       <div>
-        < Counter />
+      <Counter initialValue={10} incrementAmount= {5} incrementIntervals={1000} />
       </div>
     );
   }
