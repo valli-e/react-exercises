d9391ca58ad045e891d266900d77e0e1731840d3
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2023-03-12 17:47:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-
 import React from 'react';
-import  Welcome  from './Welcome'
-
+import  ClickTracker from './ClickTracker'
 class App extends React.Component{
   render() {
     return (
       <div>
-        < Welcome name={<strong>John</strong>} age={21} />
+        < ClickTracker/>
       </div>
     );
   }
 }
-Welcome.defaultProps = {
-  name: "Valentina"
-}
-export default App;
 
+export default App;
