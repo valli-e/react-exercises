d0320d5b6204a75c8f21afca4e0e478323dae879
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2023-03-17 18:18:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Login from './Login;
 
 export class App extends React.Component{
-  onLogin = (state) => {
-    console.log(state);
-  }
+ 
   render() {
     return (
       <div>
-        <Login onLogin= {this.onLogin}/>
+        <Login />
       </div>
     );
   }
