196c2fadaba3b92170e808373b8ff6f129ff0545
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2023-03-20 11:51:13 +0100

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,45 +1,64 @@
-// Create a Login component containing three inputs: a username input, a password input and a remember checkbox.
-//  All three inputs should be controlled components.
+// Rewrite the Login component from Forms 03 as a function component,
+//  and use the useState hook to track the state of the username, password and remember inputs. Tip: you can use useState more than once.
 
-import React, { Component } from "react";
+import React, { useState, Component } from 'react'
 
-  state = {
-    username: "",
-    password: "",
-    remember: false,
-  };
+// Define the Login component as a function that takes a props parameter.
+function Login(props) {
+   // Declare state variables using the useState hook
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false); 
+//Each state variable is declared using array destructuring
+// with the first element being the state value and the second element being a function to update the state value
+//Each variable is initialized with a default value 
 
-  handleInputs = (event) => {
-    let value = event.target.value;
-    let checked = event.target.checked;
-    let name = event.target.name;
-    console.log(checked)
-    this.setState({
-      [name]: value ?? checked
-    });
-  };
-  render() {
+
+
+  // Define event handlers for input elements
+const handleUsernameChange = (event) => {
+  setUsername(event.target.value);
+};
+
+const handlePasswordChange = (event) => {
+  setPassword(event.target.value);
+};
+
+const handleRememberChange = (event) => {
+  setRemember(event.target.checked);
+}
+
+ // Define handler for login button click
+ const handleLogin = () => {
+  // Call the onLogin prop function with the current state values
+  props.onLogin({ username, password, remember });
+}
+
+
+ // Return JSX with input elements, checkbox, and login button
     return (
       <div>
         <input
           type="text"
-          onChange={this.handleInputs}
+          onChange={handleUsernameChange}
           name="username"
           placeholder="Username"
         ></input>
         <input
           type="password"
-          onChange={this.handleInputs}
+          onChange={handlePasswordChange}
           name="password"
           placeholder="Password"
         ></input>
         <input
           type="checkbox"
-          onChange={this.handleInputs}
+          onChange={handleRememberChange}
           name="remember"
         ></input>
-        <button onClick={() => this.props.onLogin(this.state)}>Login</button>
+        <button onClick={handleLogin}>Login</button>
       </div>
     );
   }
-}
+
+
+  export default Login;
