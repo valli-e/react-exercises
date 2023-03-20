46017565b8758c67357aa9c8e09b47be7463b41d
Valentyna Hnatyuk <114984511+ValentinaHnatyuk@users.noreply.github.com>
2023-03-20 14:28:15 +0100

    Update ClickCounter.js

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,11 +1,23 @@
-import React, { useState, Component } from "react";
 
-export function ClickCounter() {
+// Add a side effect to the ClickCounter component from useState 01 
+// that calls a onCounterChange function with the current value of the counter every time value of the counter changes. The function should be received as a prop.
+
+import React, { useState,useEffect,Component } from "react";
+
+// this is a prop that represents the function that should be called everytime the counter changes
+export function ClickCounter(onCounterChange) {
   const [count, setСount] = useState(0);
 
   const handleClick = () => {
     setСount((prevCount) => prevCount + 1);
   };
+
+
+  useEffect(() => {
+    onCounterChange(count);
+  }, [count, onCounterChange]);
+
+  // we call this function with the current value of the count and we specify count and onCounterChange as dependencies for the effect to run whenever they change
 
   return (
     <div>
@@ -16,5 +28,3 @@ export function ClickCounter() {
 }
 
 export default ClickCounter;
-
-// Rewrite the ClickCounter component from Events 01 as a function component, and use the useState hook to track the state of the counter.
