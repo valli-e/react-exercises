e0ff064208fc109f18bf2107d8feb02e66d1223a
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-03-21 22:00:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import GithubUser from './GithubUser';
+import GithubUserList from './GithubUserList';
 
 
 export class App extends React.Component{
   render() {
     return (
       <div>
-      <GithubUser username="valli-e" />
+      <GithubUserList />
       </div>
     );
   }
