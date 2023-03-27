8d08c51c472a905a8e1453586e3491ce7fa7997e
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-03-27 21:52:27 +0200

    Update GithubUser.js

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,31 +1,20 @@
-import React, {useEffect, useState} from 'react'
+import useGithubUser from "./useGithubUser";
 
 export default function GithubUser({ username }) {
-    const [userData, setUserData] = useState(null);
+  const userData = useGithubUser(username);
 
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${username}`)
-        .then(response => response.json())
-        .then( data => {
-            setUserData(data)
-        })
-        .catch((error) => console.log(error))
-    }, [username])
-
-
-
-    return (
+  return (
+    <div>
+      {userData && (
         <div>
-          {userData && (
-            <div>
-              <h2>{userData.login}</h2>
-              <img src={userData.avatar_url} alt="avatar" />
-              <p>Name: {userData.name}</p>
-              <p>Followers: {userData.followers}</p>
-              <p>Following: {userData.following}</p>
-              <p>Public Repositories: {userData.public_repos}</p>
-            </div>
-          )}
+          <h2>{userData.login}</h2>
+          <img src={userData.avatar_url} alt="avatar" />
+          <p>Name: {userData.name}</p>
+          <p>Followers: {userData.followers}</p>
+          <p>Following: {userData.following}</p>
+          <p>Public Repositories: {userData.public_repos}</p>
         </div>
-      );
-    }
+      )}
+    </div>
+  );
+}
