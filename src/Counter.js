483940948e95c1cd2e957d9b086dbaac07906499
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-03-27 17:41:51 +0200

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -2,28 +2,27 @@ import React from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
 export class Counter extends React.Component {
- 
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: props.initialValue
-    }
-    setInterval(() => {
-    this.setState((prevState) => {
-      return({
-        count: prevState.count +  props.incrementAmount
-      })
-    })
-    },props.incrementIntervals)
+  state = {
+    count: this.props.initialValue
+  };
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState((prevState) => ({
+        count: prevState.count + this.props.incrementAmount
+      }));
+    }, this.props.incrementIntervals);
   }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
   render() {
     return (
       <div>
-      <CounterDisplay count={this.state.count} />
+        <CounterDisplay count={this.state.count} />
       </div>
     );
   }
 }
-
-// Extract the h1 tag showing the count state variable into a new component 
-// called CounterDisplay and render it within the Counter component, passing it the count state variable as a prop.
