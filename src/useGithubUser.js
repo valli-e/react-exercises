4be178156e2e8084cb594adc505c0bedf5c64031
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-03-27 21:55:00 +0200

    Update useGithubUser.js

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -2,15 +2,28 @@ import { useEffect, useState } from "react";
 
 export default function useGithubUser(username) {
   const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
+  const fetchUserData = () => {
+    setLoading(true);
+
     fetch(`https://api.github.com/users/${username}`)
       .then((response) => response.json())
       .then((data) => {
         setUserData(data);
+        setLoading(false);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchUserData();
   }, [username]);
 
-  return userData;
+  return [userData, fetchUserData, { error, loading }];
 }
+
