8b7032f1365d737a4bbb68b25a6d45a6222acc62
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-03-27 21:55:15 +0200

    Update GithubUser.js

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,10 +2,12 @@ import React from 'react'
 import useGithubUser from "./useGithubUser";
 
 export default function GithubUser({ username }) {
-  const userData = useGithubUser(username);
+  const [userData, fetchUserData, { error, loading }] = useGithubUser(username);
 
   return (
     <div>
+      {error && <p>Error: {error.message}</p>}
+      {loading && <p>Loading...</p>}
       {userData && (
         <div>
           <h2>{userData.login}</h2>
@@ -16,6 +18,7 @@ export default function GithubUser({ username }) {
           <p>Public Repositories: {userData.public_repos}</p>
         </div>
       )}
+      <button onClick={fetchUserData}>Refresh</button>
     </div>
   );
 }
