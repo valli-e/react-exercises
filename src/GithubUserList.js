98500fdb577e827a7f3f05f410dc4d284ab26ba6
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-03-29 16:53:27 +0200

    Update GithubUserList.js

--- src/GithubUserList.js
+++ src/GithubUserList.js
@@ -1,37 +1,46 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import GithubUser from './GithubUser';
+import ShowGithubUser from './ShowGithubUser';
 
 export default function GithubUserList() {
-    const [usernames, setUsernames] = useState([])
-    const [username, setUsername] = useState('')
+  const [usernames, setUsernames] = useState([]);
+  const [username, setUsername] = useState('');
 
+  function handleChange(event) {
+    setUsername(event.target.value);
+  }
 
-    function handleChange(event) {
-     setUsername(event.target.value)
+  function handleClick() {
+    if (username !== '') {
+      setUsernames((prevUsernames) => [...prevUsernames, username]);
     }
+    setUsername('');
+  }
 
-    function handleСlick() {
-     if(username !== '') {
-        setUsernames((prevUsernames) => [...prevUsernames, username])
-     }
-     setUsername('')
-    }
   return (
     <div>
-        <input type="text" name="username" value={username} placeholder='enter username' onChange={handleChange}/>
-        <button onClick={handleСlick}>search</button>
-       {usernames.length > 0 && 
-       <div>
-        {usernames.map((el) => {
-            return < GithubUser username={el} />
-        })
-        }
-       </div>
-        }
-
+      <input
+        type="text"
+        name="username"
+        value={username}
+        placeholder="Enter username"
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>Search</button>
+      {usernames.length > 0 && (
+        <div>
+          {usernames.map((el) => {
+            return (
+              <Link key={el} to={`/users/${el}`}>
+                {el}
+              </Link>
+            );
+          })}
+        </div>
+      )}
+      <Route path="/users/:username" component={ShowGithubUser} />
     </div>
-  )
+  );
 }
 
-//Create a GithubUserList component that maintains an array of usernames, showing a GithubUser component for each username entered. 
-//The usernames should be added to the array using an input field and a button.
