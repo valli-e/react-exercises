9bce887e287733992038ee13ba07492eae5aa1a3
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-03-29 16:45:09 +0200

    Update ClickCounter.js

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,20 +1,25 @@
-import React, { useState, Component } from "react";
+import React, { useState } from "react";
 
 export function ClickCounter() {
-  const [count, setСount] = useState(0);
+  const [count, setCount] = useState(0);
 
   const handleClick = () => {
-    setСount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
   };
 
   return (
     <div>
-      <h1> current count: {count}</h1>
-      <button onClick={handleClick}> click me </button>
+      <h1>Current count: {count}</h1>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
 
-export default ClickCounter;
-
-// Rewrite the ClickCounter component from Events 01 as a function component, and use the useState hook to track the state of the counter.
+export function Counter() {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <<ClickCounter />
+    </div>
+  );
+}
