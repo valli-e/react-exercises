7fbb806c94ea028ea27214da4a56e9930561f0c3
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-03-29 20:07:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import Form from './Form'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
-
-
-export class App extends React.Component{
-  render() {
-    return (
-      <div>
-      <Form/>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-export default App;
