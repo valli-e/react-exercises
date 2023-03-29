0901c26b10b203e061e813650996131ae1724e64
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-03-29 19:57:32 +0200

    Update useGithubUser.js

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,6 +1,10 @@
 import useSWR from 'swr';
 
 export default function useGithubUser(username) {
+  if (!username) {
+    return { userData: null };
+  }
+
   const fetcher = (url) => fetch(url).then((res) => res.json());
   const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher);
 
