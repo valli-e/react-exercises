e9191a7b059cf64d0a5d6080fd731622962b131f
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-03-30 16:54:43 +0200

    Update and rename src/LanguageSelector.js to DisplayLanguage.js

---
+++ DisplayLanguage.js
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+import { LanguageContext } from './LanguageContext';
+
+function DisplayLanguage() {
+    const text = {
+        en: {
+            title: 'You selected "English"',
+        },
+        it: {
+            title: 'Hai selezionato "Italiano"',
+        }
+    }
+
+    return <>
+    <LanguageContext.Consumer>
+        {language => {
+            return(
+                <>
+                <h1>{text[language].TITLE}</h1>
+                <p>(The value received from provider is: {language})</p>
+                </>
+            )
+        }}
+    </LanguageContext.Consumer>    
+    </>
+}
+
+export default DisplayLanguage;
